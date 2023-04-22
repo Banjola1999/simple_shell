7062df03bbadbb206d9049e9c2a777b03c4a080e
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-04-22 07:31:51 -0700

    checker

---
+++ checker.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * checker - checks whether the command is a built-in function or an executable file
+ * @cmd: tokenized user input
+ * @buf: line derived from getline function
+ *
+ * Return: 1 if cmd executed, 0 if cmd is not executed
+ */
+int checker(char **cmd, char *buf)
+{
+    /* check if the command is a built-in function */
+    if (builtins(cmd, buf)) {
+        return 1;
+    }
+
+    /* if it's not a built-in, check if it's an executable file */
+    if (access(cmd[0], X_OK) == 0)
+    {
+        exec(cmd[0], cmd);
+        return 1;
+    }
+
+    return 0;
+}
