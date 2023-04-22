8ad81f5b5f5f5ea687ee142173ad4cee1c025592
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-04-22 07:44:10 -0700

    exit command prompt

---
+++ exit.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * _exit_ - handles the exit command
+ * @command: tokenized command
+ * @line: input read from stdin
+ *
+ * This function frees the memory allocated for the input line and tokenized command,
+ * then terminates the program with a successful exit status.
+ *
+ * Return: No return value.
+ */
+void _exit_(char **command, char *line)
+{
+	free(line);
+	_free(command);
+	exit(0);
+}
