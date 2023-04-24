4f690f115263b2bb7ec3b7b43efebfdcf27f8c44
Banjola1999 <banjolaadesina@gmail.com>
2023-04-24 03:17:07 -0700

    returns the length of a string

---
+++ strlen.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string passed
+ *
+ * Return: returns length of string passed
+ */
+int _strlen(char *s)
+{
+int count = 0;
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+count++;
+}
+return (count);
+}
